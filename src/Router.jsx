f4d42b588231b463fdeedeba53a400b20be2ee04
Stella Elian <elian_stella@yahoo.com>
2026-03-12 10:35:18 +0100

    Stocker le token dans un cookie (Recommondation 3)

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,16 +1,36 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
 
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import Error404 from './pages/Error404';
+
+// LE VIGILE
+function ProtectedRoute({ children }) {
+    const token = Cookies.get('auth_token'); // Vérifie le cookie
+    if (!token) {
+        return <Navigate to="/login" />; // Pas de badge = dehors 
+    }
+    return children; // A un badge = entre 
+}
 
 export default function AppRouter() {
     return (
         <Routes>
             <Route path="/" element={<Navigate to="/login" />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/dashboard" element={<Dashboard />} />
+
+            {/*On enferme le Dashboard derrière le Vigile */}
+            <Route
+                path="/dashboard"
+                element={
+                    <ProtectedRoute>
+                        <Dashboard />
+                    </ProtectedRoute>
+                }
+            />
+
             <Route path="*" element={<Error404 />} />
         </Routes>
     );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import Cookies from 'js-cookie';
+import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
+    const navigate = useNavigate();
+
+    // Fonction de connexion
+    const handleLogin = () => {
+        // On stocke le Token dans un cookie
+        Cookies.set('auth_token', 'badge-secret-de-test');
+        // On part vers le dashboard
+        navigate('/dashboard');
+    };
+
     return (
         <div style={{ textAlign: 'center', marginTop: '100px' }}>
             <h1>Page de Connexion</h1>
-            <p>Formulaire à venir...</p>
+            <button onClick={handleLogin}>Se connecter</button>
         </div>
     );
 }
