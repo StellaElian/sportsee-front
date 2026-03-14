b22d42eb751d639dadbc6a0c689111f95937139b
Stella Elian <elian_stella@yahoo.com>
2026-03-14 16:45:58 +0100

    Debug

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
+
+//j'importe notre radio (le Contexte) pour que le Vigile puisse l'entendre !
+import { AuthContext } from './context/AuthContext';
 
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
@@ -8,13 +11,14 @@ import Error404 from './pages/Error404';
 
 // LE VIGILE
 function ProtectedRoute({ children }) {
-    const {token} = useContext(AuthContext); // Vérifie le cookie, badge présent ? 
+    const { token } = useContext(AuthContext); // Vérifie le cookie, badge présent ? 
     if (!token) {
         return <Navigate to="/login" />; // Pas de badge = dehors 
     }
     return children; // A un badge = entre 
 }
 
+//le plan
 export default function AppRouter() {
     return (
         <Routes>
