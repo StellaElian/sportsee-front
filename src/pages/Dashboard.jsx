51a351d6292ed1924ae1b72d4d6f88215ce0908e
Stella Elian <elian_stella@yahoo.com>
2026-03-14 16:31:06 +0100

    Affichage dashboard gaph 1

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,16 +1,26 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../context/AuthContext'; // On importe la radio
+import { USER_MOCK_DATA } from '../data/mock';
+import DistanceChart from '../components/DistanceChart';
+
 
 export default function Dashboard() {
     // On allume la radio pour entendre le Token et le bouton de déconnexion
     const { token, logout } = useContext(AuthContext);
+    // On prend les données du premier utilisateur pour le test (Sophie)
+    const userData = USER_MOCK_DATA[0];
+
 
     return (
-        <div style={{ textAlign: 'center', marginTop: '100px' }}>
-            <h1>Tableau de Bord 📊</h1>
-            {/* On affiche fièrement ce qu'on entend du context*/}
-            <p>Votre badge secret est : <strong>{token}</strong></p>
+        <div style={{ padding: '50px' }}>
+            {/* On utilise les vraies données de Sophie pour lui dire bonjour */}
+            <p>Vos statistiques </p>
+            <h1>{userData.userInfos.firstName} !</h1>
 
+            {/* affichage graphique + envoie de la liste "runningData" de Sophie */}
+            <DistanceChart data={userData.runningData} />
+
+            <br />
             <button onClick={logout}>Se déconnecter</button>
         </div>
     );
